c27b3241993bd7dc6aaa67e9e82825c84bab59cd
Debashish Mahato <deba.m.27@gmail.com>
2020-08-02 15:19:02 +0530

    Kotlin co-routine

--- app/src/main/java/com/demo/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/demo/kotlindemo/MainActivity.kt
@@ -2,11 +2,23 @@ package com.demo.kotlindemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch { // launch a new coroutine in background and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            Log.d("print::","Hello!")
+
+
+        }
+        Log.d("print::","World") // main thread continues while coroutine is delayed
+        Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
     }
 }
