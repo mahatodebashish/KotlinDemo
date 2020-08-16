20e944704f379a5b53a391a4c40679c9829394a9
Debashish Mahato <deba.m.27@gmail.com>
2020-08-17 02:24:06 +0530

    Threads vs Coroutines

--- app/src/main/java/com/demo/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/demo/kotlindemo/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.demo.kotlindemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,34 +23,61 @@ class MainActivity : AppCompatActivity() {
 
         // finding the textView
         var textView = findViewById(R.id.txtView) as TextView
+        var imageView = findViewById(R.id.imageView) as ImageView
 
         //With Coroutines
-      /*  GlobalScope.launch(Dispatchers.IO) {
+        textView.visibility = View.VISIBLE
+        imageView.visibility=View.GONE
 
-                    repeat(10_000){
-                        delay(1000L)
+        repeat(10_000){
+        GlobalScope.launch {
+
+                        delay(10000L)
 
                         withContext(Dispatchers.Main){
                             textView.setText(""+ Math.random())
                         }
                     }
 
-        }*/
+        }
 
         //Without Coroutines
-        val mylamda = Thread({
-            repeat (10_000){
-                Thread.sleep(1000L)
-              //  println("$x")
-                runOnUiThread { textView.setText(""+ Math.random()) }
+      /*  textView.visibility = View.VISIBLE
+        imageView.visibility=View.GONE
 
+
+        repeat(10_000){
+         thread {
+
+                    Thread.sleep(5000L)
+                    runOnUiThread {
+                        textView.setText(""+ Math.random())
+                    }
+
+                }
+            }*/
+
+
+        //Loading network image using Coroutine
+
+        /*textView.visibility = View.GONE
+        imageView.visibility=View.VISIBLE
+
+        GlobalScope.launch(Dispatchers.IO) {
+
+            val imageUrl = URL("https://developer.android.com/images/kotlin/cards/kotlin-bootcamp.png")
+
+            val httpConnection = imageUrl.openConnection() as HttpURLConnection
+            httpConnection.doInput = true
+            httpConnection.connect()
+
+            val inputStream = httpConnection.inputStream
+            val bitmapImage = BitmapFactory.decodeStream(inputStream)
+
+            launch(Dispatchers.Main) {
+                imageView.setImageBitmap(bitmapImage)
             }
-        })
-        startThread(mylamda)
-
+        }*/
     }
     }
-fun startThread(mylamda: Thread) {
-    mylamda.start()
-}
 
